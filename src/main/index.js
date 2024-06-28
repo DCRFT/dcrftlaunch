634de28db54702c7fdeaa5b7f5f4f1aeac555ec9
Nick Nicker <nick4nicker@gmail.com>
2024-06-28 18:44:01 +0200

    Improve build system, icon & resource cleanup & improvement

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,5 @@
 const {ipcRenderer, shell} = require('electron');
 const $ = require("jquery");
-const utils = require('../utils.js');
 const os = require('os');
 const path = require('node:path')
 const fs = require("node:fs");
